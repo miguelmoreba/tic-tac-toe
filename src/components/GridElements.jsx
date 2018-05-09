b3c890df1c10be8313c2651b72ad63de76be733d
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-05-09 11:26:32 +0100

    Crosses and noughts classes added

--- src/components/GridElements.jsx
+++ src/components/GridElements.jsx
@@ -10,11 +10,11 @@ class GridElements extends React.Component {
         <div id="1.2" className="cell top-row">1.2</div>
         <div id="1.3" className="cell top-row">1.3</div>
         <div id="2.1" className="cell middle-row">2.1</div>
-        <div id="2.2" className="cell middle-row">2.2</div>
+        <div id="2.2" className="cell middle-row cross">2.2</div>
         <div id="2.3" className="cell middle-row">2.3</div>
         <div id="3.1" className="cell bottom-row">3.1</div>
         <div id="3.2" className="cell bottom-row">3.2</div>
-        <div id="3.3" className="cell bottom-row">3.3</div>
+        <div id="3.3" className="cell bottom-row nought">3.3</div>
       </div>
     )
   }
