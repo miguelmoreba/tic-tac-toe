635b83c8baad82111acc0f3537f50f9561c6e8a7
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-05-09 10:17:39 +0100

    Created div for game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import GameContainer from './containers/GameContainer.jsx';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Noughts & Crosses</h1>
+        <GameContainer/>
       </div>
     );
   }
---
+++ src/containers/GameContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class GameContainer extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render(){
+    return(
+      <div>
+        This is where the game will be
+      </div>
+    )
+  }
+}
+
+export default GameContainer;
